e0b1c724d7d207f156b5606142f6611390f38105
Cheng Gu <guchengf@gmail.com>
2017-08-08 11:27:08 +0800

    update: testing

--- test.js
+++ test.js
@@ -9,12 +9,37 @@ genHash({
 const hashFile = require('./test/test-hash.js');
 
 test('gen hash file', () => {
-  fs.stat(path.relative(__dirname, './test/test-hash.js'), (err, stats) => {
-    expect(err).toBeUndefined;
-  });
+  const hashFile = fs.statSync(path.relative(__dirname, './test/test-hash.js'));
+  expect(hashFile).toBeDefined;
 });
 
 test('contain all file hash value', () => {
   expect(typeof hashFile.testIndex).toBe('string');
   expect(hashFile.testIndex.length).toBe(8);
+});
+
+test('create a copy of original file with hash in filename', () => {
+  const fileCopy = fs.statSync(path.relative(__dirname, `./test/index.${hashFile.testIndex}.js`));
+  expect(fileCopy).toBeDefined;
+});
+
+genHash({
+  config: './test-2.config.json',
+});
+
+test('gen hash file', () => {
+  const hashFile = fs.statSync(path.relative(__dirname, './test/test-hash-2.js'));
+  expect(hashFile).toBeDefined;
+});
+
+const hashFile2 = require('./test/test-hash-2.js');
+
+test('contain all file hash value', () => {
+  expect(typeof hashFile2['index-2']).toBe('string');
+  expect(hashFile2['index-2'].length).toBe(32);
+});
+
+test('create a copy of original file with hash in filename', () => {
+  const fileCopy = fs.statSync(path.relative(__dirname, `./test/index-2.${hashFile2['index-2']}.css`));
+  expect(fileCopy).toBeDefined;
 });
